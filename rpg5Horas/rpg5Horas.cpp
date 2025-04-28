7cc28d418c11c21982e5f3fb21b791828a7e1e36
TucanoiDEV <rodrigohenrique987@gmail.com>
2025-04-27 22:56:50 -0300

    Criação da função de criar classe e criação da função de criar nome

--- rpg5Horas/rpg5Horas.cpp
+++ rpg5Horas/rpg5Horas.cpp
@@ -15,7 +15,7 @@ int main() {
 	//Arrays para facilitar na programação de caracterização de personagem
 	string racas[5] = { "Humano", "Anão", "Draconiano", "Vampiro", "Elfo" };
 	string classe[5] = { "Guerreiro", "Mago", "Assassino", "Bárbaro", "Druida" };
-	string arma, escolhaRaca;
+	string arma, escolhaRaca, escolhaClasse, nomePersonagem, nomePersonagem2;
 
 	//Variáveis iniciais para mecânicas de combate do jogo
 	int vida = 0, estus = 0, estamina = 0, mana = 0, reflexo = 0, arrayEscolha = 0;
@@ -25,16 +25,17 @@ int main() {
 	cin.get();
 	system("cls");
 
-	//Nesse caso utilizo um loop pelo for para imprimir as raças na tela do jogador 
-	cout << "Escolha sua raça:\n\n";
-	for (arrayEscolha = 0; arrayEscolha < 5; arrayEscolha++) {
-
-		cout << racas[arrayEscolha] << endl;
-
-	}
-
 	//Utilizo um loop para garantir que pelo menos uma raça seja escolhida sem erros
 	do{
+
+		//Nesse caso utilizo um loop pelo for para imprimir as raças na tela do jogador 
+		cout << "Escolha sua raça:\n\n";
+		for (arrayEscolha = 0; arrayEscolha < 5; arrayEscolha++) {
+
+			cout << racas[arrayEscolha] << endl;
+
+		}
+
 		//Uso o comando cin para captar a raça do jogador
 		cout << "\n";
 		cin >> escolhaRaca;
@@ -45,19 +46,67 @@ int main() {
 		//Utilizo o if para que seja impresso uma mensagem de "seleção inválida", assim avisando ao jogador que sua raça não foi identificada
 		if (escolhaRaca != "humano" && escolhaRaca != "anão" && escolhaRaca != "draconiano" && escolhaRaca != "vampiro" && escolhaRaca != "elfo") {
 
-			cout << "Seleção inválida, por favor escolha uma das classes listadas!" << endl;
+			cout << "\n\nSeleção inválida, por favor escolha uma das classes listadas!" << endl;
+			cin.ignore();
+			cin.get();
+			system("cls");
 
 		}
 		
 	} while (escolhaRaca != "humano" && escolhaRaca != "anão" && escolhaRaca != "draconiano" && escolhaRaca != "vampiro" && escolhaRaca != "elfo");
+	system("cls");
 
-	//Agora inicio a seleção de classe a partir do mesmo loop criado para raças
-	cout << "Escolha sua classe:\n\n";
-	for (arrayEscolha = 0;arrayEscolha < 5; arrayEscolha++) {
+	//Utilizo um loop para garantir que pelo menos uma classe seja escolhida sem erros
+	do {
 
-		cout << classe[arrayEscolha] << endl;
+		//Agora inicio a seleção de classe a partir do mesmo loop criado para raças
+		cout << "Escolha sua classe:\n\n";
+		for (arrayEscolha = 0;arrayEscolha < 5; arrayEscolha++) {
 
-	}
+			cout << classe[arrayEscolha] << endl;
+
+		}
+
+		//Uso o comando cin para captar a classe do jogador
+		cout << "\n";
+		cin >> escolhaClasse;
+
+		//Utilizado para transformar todos os caracteres em minúsculo
+		transform(escolhaClasse.begin(), escolhaClasse.end(), escolhaClasse.begin(), ::tolower);
+
+		//Utilizo o if para que seja impresso uma mensagem de "seleção inválida", assim avisando ao jogador que sua classe não foi identificada
+		if (escolhaClasse != "guerreiro" && escolhaClasse != "mago" && escolhaClasse != "assassino" && escolhaClasse != "bárbaro" && escolhaClasse != "druida") {
+
+			cout << "\n\nSeleção inválida, por favor escolha uma das classes listadas!" << endl;
+			cin.ignore();
+			cin.get();
+			system("cls");
+
+		}
+
+	} while (escolhaClasse != "guerreiro" && escolhaClasse != "mago" && escolhaClasse != "assassino" && escolhaClasse != "bárbaro" && escolhaClasse != "druida");
+	system("cls");
+
+	//Utilizo um loop "do-while" para descobrir o nome do personagem do jogador e garantir que o nome foi digitado da maneira correta
+	do{
+
+		//Faço a verificação a partir de duas perguntas na esperança que o jogador responda o mesmo que elas para assim gerar o fim do loop
+		cout << "Digite o nome do seu personagem: ";
+		cin >> nomePersonagem;
+		cout << "\nConfirme o nome do seu personagem: ";
+		cin >> nomePersonagem2;
+
+		//Utilizo um if para informar que a confirmação de nome está diferente
+		if (nomePersonagem != nomePersonagem2) {
+
+			cout << "\n\nOs nomes digitados são diferentes.\nPor favor insira o mesmo nome e lembre-se de levar em consideração letras maiúsculas e minúsculas!";
+			cin.ignore();
+			cin.get();
+			system("cls");
+
+		}
+
+	} while (nomePersonagem != nomePersonagem2);
 
 		return 0;
 }
